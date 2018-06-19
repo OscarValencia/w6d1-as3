930e88b703878e25a4fe5e49da29adda538f72d1
unknown <oscar.valencia@mobileappscompany.us>
2018-06-18 21:41:53 -0400

    complete

---
+++ app/src/main/java/com/oscarv/w6d1_as3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.oscarv.w6d1_as3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
